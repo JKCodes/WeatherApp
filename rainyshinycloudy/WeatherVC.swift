ef65c662ecddef1dd5888b4d93814017a4fc2b55
Joe Kim <josephkim0224@gmail.com>
2017-01-26 09:27:02 -0500

    Initial Setup

---
+++ rainyshinycloudy/WeatherVC.swift
@@ -0,0 +1,18 @@
+//
+//  WeatherVC.swift
+//  rainyshinycloudy
+//
+//  Created by Joseph Kim on 1/26/17.
+//  Copyright © 2017 Joseph Kim. All rights reserved.
+//
+
+import UIKit
+
+class WeatherVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
+
